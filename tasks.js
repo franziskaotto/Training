cca2afd0ab1fdf68d9f0254237f342aa0ee96626
Franziska Otto <franziska.otto@gmx.at>
2023-05-28 18:06:52 +0200

    added some changes

--- tasks.js
+++ tasks.js
@@ -1,8 +1,55 @@
+/*
 let numberArray = []
 let amount = 0
 let text = "divisible by 7 or 5 or 3!"
 
 for (i = 0; i < 10; i++) { 
+  numberArray.push(Math.floor(Math.random() * 99))
+
+  if (numberArray[i] % 7 === 0 || numberArray[i] % 5 === 0 || numberArray[i] % 3 === 0) {
+    amount += 1
+    console.log(`${numberArray[i]} is ${text}`);
+  }
+}
+console.log(`We found ${amount} numbers ${text}`);
+
+*/
+
+
+//this is how result need to look like (list of 3 objcets with two key-value pairs)
+//let resultList = [{name: "Adri", points: 111}, {name: "Heidelinde", points: 4943}, {name: "Martin", points: 23}]
+
+let inputArray = ["Adri             111", "Heidelinde       4943", "Martin           23"]
+
+
+
+
+function fromArrayToObject (anArray) {
+  
+  let resultArray = []
+  
+  for (let i = 0; i < anArray.length; i++) {
+    let splittedArray = anArray[i].split(` `)
+    //console.log(splittedArray)
+
+    let valueName = splittedArray[0];
+    let valuePoints = parseInt(splittedArray.length -1);
+
+    let anObject = {name: valueName, points: valuePoints}
+    resultArray.push(anObject);
+  }
+  console.log(resultArray)
+  return resultArray
+}
+fromArrayToObject(inputArray)
+
+
+
+let numberArray = []
+let amount = 0
+let text = "divisible by 7 or 5 or 3!"
+
+for (let i = 0; i < 10; i++) { 
   numberArray.push(Math.floor(Math.random() * 99))
 
   if (numberArray[i] % 7 === 0 || numberArray[i] % 5 === 0 || numberArray[i] % 3 === 0) {
