2cecfd4f72efe0d9ba01db47c74e7eaf862055ce
Franziska Otto <franziska.otto@gmx.at>
2023-06-19 22:19:43 +0200

    hes

--- 8Arraymethods.js
+++ 8Arraymethods.js
@@ -49,8 +49,6 @@ console.log(foundItem);
 
 */
 
-
-
 //forEach_Method
 console.log("_______________FOR EACH________________________");
 items.forEach((item) => {                                     //it´s going to do whats inside this function for every singe item
@@ -67,7 +65,7 @@ const hasInexpensiveItems = items.some((items) => {               //checks for s
 
 console.log(hasInexpensiveItems);
 
-//Some_Method - returns true or false
+//Every_Method - returns true or false
 console.log("_______________EVERY________________________");
 const hasExpensiveItems = items.every((items) => {               //checks for every Item
   return items.price <= 0  //1000                                //As soon as a SINGLE item returns true, it returns true!
@@ -75,3 +73,39 @@ const hasExpensiveItems = items.every((items) => {               //checks for ev
 })
 
 console.log(hasExpensiveItems);
+
+
+//Some_Method - returns true or false
+console.log("_______________REDUCE________________________");
+
+//is a bit different. Normaly we do a addition of the price with a for loop and add each price and log it.
+
+const total = items.reduce((currentTotal, item) => {        //currentTotal is the item we are iterating over
+return item.price + currentTotal
+}, 0/*second Parameter, starting point*/  )
+
+console.log(total);
+
+/*i: break it down: reduce runs a function on every single item in the array
+0 is like the "let sum = 0" -> here the addition starts and "currentTotal=0"
+in the next iteration 0 becomes 100 and currentTotal = 100, so the next 200 gets added, and the 100 becomes 200.
+
+
+const total = items.reduce((0, item) => {        //currentTotal is the item we are iterating over
+return item.price + currentTotal
+}, 0  )
+const total = items.reduce((100, item) => {       
+return item.price + currentTotal
+}, 0 )
+const total = items.reduce((200, item) => {       
+},0)
+*/
+
+
+//Some_Method - returns true or false
+console.log("_______________INCLUDES________________________");
+
+const items = [1, 2, 3, 4, 5]
+
+const includesTwo = items.includes(2)               //returns TRUE or FALSE
+console.log(includesTwo);
